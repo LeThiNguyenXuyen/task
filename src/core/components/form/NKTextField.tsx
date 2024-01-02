22b97ebc59132d18f3705e88ae1761fceac61db1
Steven <Lucct9389@gmail.com>
2024-01-02 18:10:21 +0700

    update

--- src/core/components/form/NKTextField.tsx
+++ src/core/components/form/NKTextField.tsx
@@ -6,48 +6,62 @@ import { Controller, useFormContext } from "react-hook-form";
 import NKFieldWrapper from "./NKFieldWrapper";
 
 export enum NKTextFieldTheme {
-    DEFAULT = "DEFAULT",
-    AUTH = "AUTH",
+  DEFAULT = "DEFAULT",
+  AUTH = "AUTH",
 }
 
 interface NKTextFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
-    name: string;
-    label: string;
-    isShow?: boolean;
-    labelClassName?: string;
-    extraProps?: any;
-    theme?: NKTextFieldTheme;
-    icon?: React.ReactNode;
+  name: string;
+  label: string;
+  isShow?: boolean;
+  labelClassName?: string;
+  extraProps?: any;
+  theme?: NKTextFieldTheme;
+  icon?: React.ReactNode;
 }
 
-const NKTextField: React.FC<NKTextFieldProps> = ({ name, isShow = true, label, labelClassName, theme = NKTextFieldTheme.DEFAULT, icon, ...rest }) => {
-    const formMethods = useFormContext();
+const NKTextField: React.FC<NKTextFieldProps> = ({
+  name,
+  isShow = true,
+  label,
+  labelClassName,
+  theme = NKTextFieldTheme.DEFAULT,
+  icon,
+  ...rest
+}) => {
+  const formMethods = useFormContext();
 
-    return (
-        <NKFieldWrapper className={labelClassName} isShow={isShow} label={label} name={name}>
-            <Controller
-                name={name}
-                control={formMethods.control}
-                render={({ field }) => (
-                    <div
-                        className={clsx([], {
-                            "": theme === NKTextFieldTheme.AUTH,
-                        })}
-                    >
-                        {icon}
-                        <input
-                            {...field}
-                            {...rest}
-                            className={clsx(["w-full focus:outline-none"], {
-                                "block w-full rounded-xl border border-gray-100  py-3 text-gray-900 shadow-sm focus:outline-none bg-[#F3F4F6] placeholder:text-gray-400  sm:text-sm text-sm sm:leading-6 px-4":
-                                    theme === NKTextFieldTheme.AUTH,
-                            })}
-                        />
-                    </div>
-                )}
+  return (
+    <NKFieldWrapper
+      className={labelClassName}
+      isShow={isShow}
+      label={label}
+      name={name}
+    >
+      <Controller
+        name={name}
+        control={formMethods.control}
+        render={({ field }) => (
+          <div
+            className={clsx([], {
+              "border p-3 items-center justify-center  bg-[#F3F4F6] placeholder:text-gray-400  flex rounded-lg gap-2 border-gray-100 text-gray-900 shadow-sm":
+                theme === NKTextFieldTheme.AUTH,
+            })}
+          >
+            {icon}
+            <input
+              {...field}
+              {...rest}
+              className={clsx(["w-full focus:outline-none"], {
+                " w-full focus:outline-none border-none ":
+                  theme === NKTextFieldTheme.AUTH,
+              })}
             />
-        </NKFieldWrapper>
-    );
+          </div>
+        )}
+      />
+    </NKFieldWrapper>
+  );
 };
 
 export default NKTextField;
