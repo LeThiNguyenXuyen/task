40804d1d8bad1b39f11a8bb844a5a8fa49a34991
unknown <kainesv86@gmail.com>
2024-01-07 22:19:37 +0700

    Add user profile

--- src/app/app/post/user/[id]/page.tsx
+++ src/app/app/post/user/[id]/page.tsx
@@ -4,63 +4,54 @@ import * as React from 'react';
 
 import Link from 'next/link';
 
-import { AiOutlineArrowLeft } from 'react-icons/ai';
-import { HiOutlineLocationMarker } from 'react-icons/hi';
-import { useSelector } from 'react-redux';
+import { ArrowLeft } from 'akar-icons';
+import { MdHomeFilled } from 'react-icons/md';
 
 import { NKRouter } from '@/core/NKRouter';
-import { userPostApi } from '@/core/api/user-post.api';
-import PostCard from '@/core/components/post/PostCard';
-import ScrollInfinityBuilder from '@/core/components/scroll/ScrollInfinityBuilder';
-import { RootState } from '@/core/store';
-import { UserState } from '@/core/store/user';
 
 interface UserProfilePageProps {}
 
 const UserProfilePage: React.FunctionComponent<UserProfilePageProps> = () => {
     return (
-        <div className="fade-in flex w-[600px] flex-shrink-0 flex-col bg-white pb-10">
-            {/* <div className="flex h-[52px] w-full items-center justify-start gap-9 px-4 text-black">
-                <Link href={NKRouter.app.post.index()} className="text-xl">
-                    <AiOutlineArrowLeft />
-                </Link>
-                <div className="flex flex-col">
-                    <p className="text-lg font-bold">{name}</p>
-                    <p className="text-xs font-medium">9 posts</p>
-                </div>
+        <div className="fade-in relative flex h-full w-full flex-shrink-0 flex-col items-start justify-start bg-white">
+            <Link className="absolute left-4 top-4 z-10" href={NKRouter.app.post.index()}>
+                <button className="flex h-10 w-10 items-center justify-center rounded-full bg-white">
+                    <ArrowLeft className="h-8 w-8 text-black" />
+                </button>
+            </Link>
+            <Link className="absolute right-4 top-4 z-10" href={NKRouter.app.post.index()}>
+                <button className="flex h-10 w-10 items-center justify-center rounded-full bg-white">
+                    <MdHomeFilled className="h-8 w-8 text-black" />
+                </button>
+            </Link>
+            <div className="h-56 w-full flex-shrink-0">
+                <img className="h-full w-full object-cover" src="https://picsum.photos/200/300" />
             </div>
-            <div className="relative z-10 h-[200px] w-full">
-                <img className="h-full w-full object-cover object-bottom" src="https://images3.alphacoders.com/880/880765.png" />
-                <figure className="absolute -bottom-16 left-3 h-36 w-36 overflow-hidden rounded-full border-4 border-[#17202A] bg-white">
-                    <img
-                        className="h-full w-full object-cover"
-                        src={`https://ui-avatars.com/api/?name=${name}&background=0D8ABC&color=fff&size=32`}
-                    />
-                </figure>
-            </div>
-            <div className="relative z-0 flex w-full flex-col bg-white px-[14px] pb-3 pt-16">
-                <Link
-                    href={NKRouter.app.post.profile()}
-                    className="absolute right-4 top-2.5 rounded-full border border-black px-5 py-2.5 text-base font-bold"
-                >
-                    Chỉnh sửa
-                </Link>
-                <p className="text-xl font-bold text-[#0F1419]">{name}</p>
-                <p className="text-base font-medium text-black">@{name.toLocaleLowerCase().split(' ').join('-')}</p>
-                <p className="mt-1.5 text-base font-medium text-[#0F1419]">Nhà sáng tạo nội dung</p>
-                <div className="mt-1.5 flex items-center text-base font-medium text-black">
-                    <div className="text-lg">
-                        <HiOutlineLocationMarker />
+            <div className="relative h-full w-full bg-[#E6EEFA] shadow">
+                <div className="absolute -top-10 left-0 flex h-10 w-full justify-center gap-11 rounded-t-[64px] bg-inherit px-4 pt-8">
+                    <div className="flex w-full flex-col items-center justify-center text-black">
+                        <p className="text-base font-bold">12</p>
+                        <p className="text-sm font-medium">người theo dõi</p>
                     </div>
-                    <p>Việt Nam</p>
+                    <div className="flex w-full flex-col items-center justify-center text-black">
+                        <p className="text-base font-bold">12</p>
+                        <p className="text-sm font-medium">người theo dõi</p>
+                    </div>
+                </div>
+                <div className="absolute -top-[84px] left-1/2 h-24 w-24 -translate-x-1/2 overflow-hidden rounded-full border-4 border-white">
+                    <img className="h-full w-full object-cover" src="https://picsum.photos/200/300" />
+                </div>
+                <div className="flex h-full w-full flex-col items-center px-4 pt-10 text-center">
+                    <p className="mb-1 text-base font-bold text-black">@kaine.sv</p>
+                    <p className="mb-3  text-sm text-gray-500">
+                        My name is Prathyaksh. I like dancing in the rain and travelling all around the world.
+                    </p>
+                    <div className="flex gap-6">
+                        <button className="shadow-2xls rounded-full bg-[#3C91D3] px-6 py-2 text-base text-white">Theo dõi</button>
+                        <button className="rounded-full bg-white px-6 py-2 text-base text-black shadow-2xl">Nhắn tin</button>
+                    </div>
                 </div>
             </div>
-            <ScrollInfinityBuilder
-                className="my-2 flex !w-full flex-col gap-4"
-                queryApi={userPostApi.v1Get}
-                sourceKey="userPostApi.v1Get"
-                render={(item, index) => <PostCard data={item} key={item.id} className="shadow" />}
-            /> */}
         </div>
     );
 };
